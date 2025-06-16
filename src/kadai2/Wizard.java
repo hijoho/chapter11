413e8c23888cc01a2d83be14c2749b641d5066e6
241007 <241007@std.hi-joho.ac.jp>
2025-06-16 12:37:55 +0900

    課題2

--- src/kadai2/Wizard.java
+++ src/kadai2/Wizard.java
@@ -1,2 +1,9 @@
 package kadai2;
-public class Wizard extends Character {}
+public class Wizard extends Character {
+    public void attack (Matango m){
+        System.out.println(this.name+"の攻撃！");
+        System.out.println("魔法使いは火の玉を放った！");
+        System.out.println("敵に7のダメージを与えた");
+        m.hp -= 7;
+    }
+}
