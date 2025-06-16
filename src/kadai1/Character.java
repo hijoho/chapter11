a74e1500c9e589d6e49c0a96afbdb61ed8f7009f
241007 <241007@std.hi-joho.ac.jp>
2025-06-16 12:06:24 +0900

    エラー課題1

---
+++ src/kadai1/Character.java
@@ -0,0 +1,9 @@
+package kadai1;
+public abstract class Character {
+    String name;
+    int hp;
+    public abstract void attack(Matango m);
+    public void run(){
+        System.out.println(this.name+"は逃げ出した！");
+    }
+}
